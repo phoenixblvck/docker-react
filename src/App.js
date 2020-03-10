5e29f87707823e1d868ce0a28f3740b9766d6307
Andries Bingani <andries.bingani@investec.co.za>
2020-03-10 13:48:50 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi there!
+         My react Application.
         </p>
         <a
           className="App-link"
